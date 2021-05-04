0f021c63dfdc949887bd4f9ba8dcc63fa0761d29
sepezho <sepezho@gmail.com>
2021-05-05 01:15:00 +0300

    Added https to proxy

--- Back/CORS_proxy/CORS_proxy.js
+++ Back/CORS_proxy/CORS_proxy.js
@@ -1,8 +1,21 @@
-var host = process.env.HOST || '46.101.118.222';
+var cors_proxy = require('cors-anywhere');
+var fs = require('fs');
+
+var host = process.env.HOST || 'sepezho.com';
 var port = process.env.PORT || 5555;
 
-var cors_proxy = require('cors-anywhere');
+const privateKey = fs.readFileSync(
+    "/home/admin/conf/web/ssl.sepezho.com.key",
+    "utf8"
+  );
+const certificate = fs.readFileSync(
+    "/home/admin/conf/web/ssl.sepezho.com.crt",
+    "utf8"
+  );
+const credentials = { key: privateKey, cert: certificate };
+
 cors_proxy.createServer({
+    httpsOptions: credentials,
     originWhitelist: [],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
--- Front/src/Components/RenderImg.tsx
+++ Front/src/Components/RenderImg.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Img from "react-cool-img";
 
 const RenderImg: React.FC = ({url}) => {
-  return <Img src={'http://46.101.118.222:5555/'+url} crossOrigin="anonymous" lazy={false} />
+  return <Img src={'https://sepezho.com:5555/'+url} crossOrigin="anonymous" lazy={false} />
 }
 
 export default RenderImg
