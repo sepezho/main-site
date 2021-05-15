b194f80815f699994b4c291d768a4c4d86579770
sepezho <sepezho@gmail.com>
2021-05-15 22:16:42 +0300

    Some edits.

--- Front/src/Components/Portfolio/Bubbles/Bubble/index.jsx
+++ Front/src/Components/Portfolio/Bubbles/Bubble/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Tilt from '../../../../Logics/Tilt';
+import useResponsive from '../../../../Logics/Responsive';
 
 import s from './Style/Bubble.module.sass';
 
@@ -6,15 +8,23 @@ const Bubble = props => {
   const styles = {
     width: props.size,
     height: props.size,
-    border: `6px solid ${props.isCommercial ? '#BCD0E3' : '#BAE5D6'}`,
+    boxShadow: `8px 8px 16px ${props.isCommercial ? '#BAE5D6' : '#F8C9D5'}, inset -8px -8px 16px ${props.isCommercial ? '#BAE5D6' : '#F8C9D5'}`,
 
     ...props.style
   }
+  
   return (
     <div className={s.container} style={styles}>
-      <img src={props.cover} className={s.cover} alt='#'/>
-    </div> 
-  );
+      {
+        useResponsive('(min-width: 950px)', true) ?
+          <Tilt children={
+            <img src={props.cover} className={s.cover} alt='#'/>
+          } />
+        :
+          <img src={props.cover} className={s.cover} alt='#'/>
+      }
+    </div>
+  )
 }
 
 export default Bubble;
--- Front/src/Components/Portfolio/Bubbles/index.jsx
+++ Front/src/Components/Portfolio/Bubbles/index.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Bubble from './Bubble';
 
 import PumpyFarmCover from '../../../Static/Img/Portfolio/PumpyFarmCover.png';
 import s from './Style/Bubbles.module.sass';
 
-const bubblesData = [
+const compressedBubles = [
   {key: 0, cover: PumpyFarmCover, isCommercial: true, size: 300, style: {margin: '0 300px 0 0'}},
   {key: 1, cover: PumpyFarmCover, isCommercial: true, size: 300, style: {margin: '80px 0 0 300px'}},
   {key: 2, cover: PumpyFarmCover, isCommercial: true, size: 300, style: {margin: '300px 140px 0 0'}},
@@ -18,19 +18,36 @@ const bubblesData = [
   {key: 10, cover: PumpyFarmCover, isCommercial: false, size: 300, style: {margin: '942px 294px 0 0'}},
 ]
 
+const bubles = [
+  {key: 0, cover: PumpyFarmCover, isCommercial: true, size: 300, style: {margin: '0 315px 0 0'}},
+  {key: 1, cover: PumpyFarmCover, isCommercial: true, size: 300, style: {margin: '95px 0 0 315px'}},
+  {key: 2, cover: PumpyFarmCover, isCommercial: true, size: 300, style: {margin: '320px 160px 0 0'}},
+  {key: 3, cover: PumpyFarmCover, isCommercial: true, size: 200, style: {margin: '410px 0 0 400px'}},
+  {key: 4, cover: PumpyFarmCover, isCommercial: true, size: 150, style: {margin: '600px 0 0 150px'}},
+  {key: 5, cover: PumpyFarmCover, isCommercial: false, size: 190, style: {margin: '630px 0 0 550px'}},
+  {key: 6, cover: PumpyFarmCover, isCommercial: false, size: 150, style: {margin: '650px 170px 0 0'}},
+  {key: 7, cover: PumpyFarmCover, isCommercial: false, size: 200, style: {margin: '600px 540px 0 0'}},
+  {key: 8, cover: PumpyFarmCover, isCommercial: false, size: 300, style: {margin: '780px 0 0 190px'}},
+  {key: 9, cover: PumpyFarmCover, isCommercial: false, size: 200, style: {margin: '800px 350px 0 0'}},
+  {key: 10, cover: PumpyFarmCover, isCommercial: false, size: 300, style: {margin: '1020px 250px 0 0'}},
+]
+
+
 const Bubbles = () => {
+  const [bubblesData, updateBubblesData] = useState(compressedBubles)
+  
+  setTimeout(()=>{
+    updateBubblesData(bubles)
+  }, 1000)
+
+  // if document.documentElement.scrollTop
 
   return (
     <div className={s.container}>
-      
       <div className={s.bubblesContainer}>
         {bubblesData.map(e=>
           <Bubble
-            key={e.key}
-            cover={e.cover}
-            isCommercial={e.isCommercial}
-            size={e.size}
-            style={e.style}
+            {...e}
           />
         )}
       </div>
